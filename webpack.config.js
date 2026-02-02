68eec5702746f28421f19ebbe2853ca587a88bc7
kodonnell <kodonnell@users.noreply.github.com>
2026-02-02 16:29:14 +1300

    try that

--- webpack.config.js
+++ webpack.config.js
@@ -14,9 +14,10 @@ module.exports = {
     resolve: {
         alias: {
             'process/browser': path.resolve(__dirname, 'node_modules/process/browser.js'),
+            'process/browser.js': path.resolve(__dirname, 'node_modules/process/browser.js'),
         },
         fallback: {
-            "process": require.resolve("process/browser"),
+            "process": require.resolve("process/browser.js"),
         }
     }
 };
